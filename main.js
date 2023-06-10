9b0033ee1ce49191e625d45578fed8141aeed4d4
Bryce Capuano <bryce.capuano@gmail.com>
2023-06-10 10:16:19 -0400

    hello

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("aboutB").addEventListener("click", aboutPage);
+
+function aboutPage() {
+    window.location.href = "about.html";
+    document.getElementById("aboutB").innerHTML = "AAHHh"
+}
